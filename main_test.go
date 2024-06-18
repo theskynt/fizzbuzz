b727566259bd226aeebc83d5a989ce0b04b425ec
Narunart Tuntue <narunart.t@arise.tech>
2024-06-18 21:37:03 +0700

    [SKY] should return 13 when input is 13

--- main_test.go
+++ main_test.go
@@ -111,4 +111,13 @@ func TestFizzbuzz(t *testing.T) {
 		}
 	})
 	
+	t.Run("should return 13 when input is 13", func(t *testing.T) {
+		input := 13
+		output := fizzbuzz(input)
+		want := "13"
+		if output != want {
+			t.Errorf("want %v , but output %v", want, output)
+		}
+	})
+	
 }
