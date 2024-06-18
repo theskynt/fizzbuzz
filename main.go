fc83824658250dd80cf4cc8cd09f9e72d7ccbe30
Narunart Tuntue <narunart.t@arise.tech>
2024-06-18 21:40:10 +0700

    [SKY] should return Fizzbuzz when input is 15

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 }
 
 func fizzbuzz(input int) string {
+
+	if input%3 == 0 && input%5 == 0 {
+		return "Fizzbuzz"
+	}
 	if input%3 == 0 {
 		return "Fizz"
 	}
--- main_test.go
+++ main_test.go
@@ -129,4 +129,13 @@ func TestFizzbuzz(t *testing.T) {
 		}
 	})
 
+	t.Run("should return Fizzbuzz when input is 15", func(t *testing.T) {
+		input := 15
+		output := fizzbuzz(input)
+		want := "Fizzbuzz"
+		if output != want {
+			t.Errorf("want %v , but output %v", want, output)
+		}
+	})
+
 }
