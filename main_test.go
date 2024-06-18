7ead5f7ed9b2a7142e2414819427b6c650738b04
Narunart Tuntue <narunart.t@arise.tech>
2024-06-18 21:46:59 +0700

    [SKY, 2] should return Buzz when input is 5

--- main_test.go
+++ main_test.go
@@ -29,4 +29,22 @@ func TestFizzbuzz(t *testing.T) {
 			t.Errorf("want %v , but output %v", want, output)
 		}
 	})
+
+	t.Run("should return 4 when input is 4", func(t *testing.T) {
+		input := 4
+		output := fizzbuzz(input)
+		want := "4"
+		if output != want {
+			t.Errorf("want %v , but output %v", want, output)
+		}
+	})
+
+	t.Run("should return Buzz when input is 5", func(t *testing.T) {
+		input := 5
+		output := fizzbuzz(input)
+		want := "Buzz"
+		if output != want {
+			t.Errorf("want %v , but output %v", want, output)
+		}
+	})
 }
