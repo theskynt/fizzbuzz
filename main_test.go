d527763a2201cbe1bfe3a9fdda44633c884852c9
Narunart Tuntue <narunart.t@arise.tech>
2024-06-18 21:44:38 +0700

    [SKY, 2] should return 2 when input is 2

--- main_test.go
+++ main_test.go
@@ -11,4 +11,15 @@ func TestFizzbuzz(t *testing.T) {
 			t.Errorf("want %v , but output %v", want, output)
 		}
 	})
+
+	t.Run("should return 2 when input is 2", func(t *testing.T) {
+		input := 2
+		output := fizzbuzz(input)
+		want := "2"
+		if output != want {
+			t.Errorf("want %v , but output %v", want, output)
+		}
+	})
+
+	
 }
