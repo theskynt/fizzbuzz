b88c85f03a71afb2725e0e063a077a0e5e62b2c1
Narunart Tuntue <narunart.t@arise.tech>
2024-06-19 22:14:20 +0700

    [FizzBuzz with ONE if no switch statement] should return Buzz when input is 5

--- main_test.go
+++ main_test.go
@@ -58,4 +58,18 @@ func TestFizzbuzz(t *testing.T) {
 			t.Errorf("want %v , but got %v", want, got)
 		}
 	})
+
+	t.Run("should return Buzz when input is 5", func(t *testing.T) {
+		// Arrange
+		input := 4
+		want := "4"
+
+		// Act
+		got := fizzbuzz(input)
+
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 }
