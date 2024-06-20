671aca2eb43c1c12701c540bcdc083e0abf569c9
Narunart Tuntue <narunart.t@arise.tech>
2024-06-20 21:41:39 +0700

    [FizzBuzz with NO if and NO switch statement] should return 1 when input is 1

--- main.go
+++ main.go
@@ -13,8 +13,5 @@ func fizzbuzz(input int) string {
 	result := ""
     result += map[bool]string{true: "Fizz"}[input%3 == 0]
     result += map[bool]string{true: "Buzz"}[input%5 == 0]
-    if result == "" {
-        result = strconv.Itoa(input)
-    }
-	return result
+	return map[bool]string{true: result, false: strconv.Itoa(input)}[result != ""]
 }
