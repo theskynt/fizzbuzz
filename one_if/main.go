51d5ae18d1cd3cfaacd0aa946f68bf746342abef
Narunart Tuntue <narunart.t@arise.tech>
2024-06-24 23:31:37 +0700

    [one if FizzBuzz] should return Buzz when input is 5

--- one_if/main.go
+++ one_if/main.go
@@ -12,15 +12,9 @@ func main() {
 func fizzbuzz(input int) string {
 	got := ""
 	got += map[bool]string{true: "Fizz"}[input%3 == 0]
+	got += map[bool]string{true: "Buzz"}[input%5 == 0]
 	if got == ""{
 		got = strconv.Itoa(input)
 	}
 	return got
-	// result := ""
-    // result += map[bool]string{true: "Fizz"}[input%3 == 0]
-    // result += map[bool]string{true: "Buzz"}[input%5 == 0]
-    // if result == "" {
-    //     result = strconv.Itoa(input)
-    // }
-	// return result
 }
