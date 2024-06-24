eaa7b40bf31bea99788e86f29303f946ba711099
Narunart Tuntue <narunart.t@arise.tech>
2024-06-24 23:41:32 +0700

    [one if FizzBuzz] should return 14 when input is 14

--- one_if/main_test.go
+++ one_if/main_test.go
@@ -184,4 +184,18 @@ func TestFizzbuzz(t *testing.T) {
 			t.Errorf("want %v , but got %v", want, got)
 		}
 	})
+
+	t.Run("should return 14 when input is 14", func(t *testing.T) {
+		// Arrange
+		input := 14
+		want := "14"
+
+		// Act
+		got := fizzbuzz(input)
+
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 }
