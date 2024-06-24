516d894a8772ed0449afa35c4b646a640a7b955e
Narunart Tuntue <narunart.t@arise.tech>
2024-06-24 23:27:26 +0700

    [one if FizzBuzz] should return 1 when input is 1

---
+++ one_if/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Hello Go")
+}
+
+func fizzbuzz(input int) string {
+	got := ""
+	// got += map[bool]string{true: "Fizz"}[input%3 == 0]
+	if got == ""{
+		got = strconv.Itoa(input)
+	}
+	return got
+	// result := ""
+    // result += map[bool]string{true: "Fizz"}[input%3 == 0]
+    // result += map[bool]string{true: "Buzz"}[input%5 == 0]
+    // if result == "" {
+    //     result = strconv.Itoa(input)
+    // }
+	// return result
+}
