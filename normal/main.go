80d90901af16338f6a7d8f75035fb679027cb8f9
Narunart Tuntue <narunart.t@arise.tech>
2024-06-25 22:16:48 +0700

    [normal FizzBuzz] should return Buzz when input is 5

--- normal/main.go
+++ normal/main.go
@@ -16,8 +16,8 @@ func fizzbuzz(input int) string {
 	if input%3 == 0 {
 		return "Fizz"
 	}
-	// if input%5 == 0 {
-	// 	return "Buzz"
-	// }
+	if input%5 == 0 {
+		return "Buzz"
+	}
 	return strconv.Itoa(input)
 }
