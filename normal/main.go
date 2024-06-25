177124972aa826214074d49357b0cf8022ac0a73
Narunart Tuntue <narunart.t@arise.tech>
2024-06-25 22:15:41 +0700

    [normal FizzBuzz] should return Fizz when input is 3

--- normal/main.go
+++ normal/main.go
@@ -13,9 +13,9 @@ func fizzbuzz(input int) string {
 	// if input%3 == 0 && input%5 == 0 {
 	// 	return "FizzBuzz"
 	// }
-	// if input%3 == 0 {
-	// 	return "Fizz"
-	// }
+	if input%3 == 0 {
+		return "Fizz"
+	}
 	// if input%5 == 0 {
 	// 	return "Buzz"
 	// }
--- normal/main_test.go
+++ normal/main_test.go
@@ -31,19 +31,19 @@ func TestFizzbuzz(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return Fizz when input is 3", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := 3
-	// 	want := "Fizz"
+	t.Run("should return Fizz when input is 3", func(t *testing.T) {
+		// Arrange
+		input := 3
+		want := "Fizz"
 
-	// 	// Act
-	// 	got := fizzbuzz(input)
+		// Act
+		got := fizzbuzz(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return 4 when input is 4", func(t *testing.T) {
 	// 	// Arrange
