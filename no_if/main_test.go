cea94c6068bb910ffacdb44b15592981ba81c028
Narunart Tuntue <narunart.t@arise.tech>
2024-06-25 22:42:54 +0700

    [no if FizzBuzz] should return 14 when input is 14

--- no_if/main_test.go
+++ no_if/main_test.go
@@ -185,19 +185,19 @@ func TestFizzbuzz(t *testing.T) {
 		}
 	})
 
-	// t.Run("should return 14 when input is 14", func(t *testing.T) {
-	// 	// Arrange
-	// 	input := 14
-	// 	want := "14"
+	t.Run("should return 14 when input is 14", func(t *testing.T) {
+		// Arrange
+		input := 14
+		want := "14"
 
-	// 	// Act
-	// 	got := fizzbuzz(input)
+		// Act
+		got := fizzbuzz(input)
 
-	// 	// Assert
-	// 	if got != want {
-	// 		t.Errorf("want %v , but got %v", want, got)
-	// 	}
-	// })
+		// Assert
+		if got != want {
+			t.Errorf("want %v , but got %v", want, got)
+		}
+	})
 
 	// t.Run("should return FizzBuzz when input is 15", func(t *testing.T) {
 	// 	// Arrange
