ed773b4bd1f05605e71b3ab618a63151abd1e208
Narunart Tuntue <narunart.t@arise.tech>
2024-06-25 22:14:33 +0700

    [normal FizzBuzz] should return 1 when input is 1

--- normal/main.go
+++ normal/main.go
@@ -10,14 +10,14 @@ func main() {
 }
 
 func fizzbuzz(input int) string {
-	if input%3 == 0 && input%5 == 0 {
-		return "FizzBuzz"
-	}
-	if input%3 == 0 {
-		return "Fizz"
-	}
-	if input%5 == 0 {
-		return "Buzz"
-	}
+	// if input%3 == 0 && input%5 == 0 {
+	// 	return "FizzBuzz"
+	// }
+	// if input%3 == 0 {
+	// 	return "Fizz"
+	// }
+	// if input%5 == 0 {
+	// 	return "Buzz"
+	// }
 	return strconv.Itoa(input)
 }
