c1faa7415af94d45f8fa2c4034d32394bab45f02
Narunart Tuntue <narunart.t@arise.tech>
2024-06-26 23:18:06 +0700

    [no if FizzBuzz] should return Fizz when input is 3

--- no_if/main.go
+++ no_if/main.go
@@ -11,7 +11,7 @@ func main() {
 
 func fizzbuzz(input int) string {
 	result := ""
-    // result += map[bool]string{true: "Fizz"}[input%3 == 0]
+    result += map[bool]string{true: "Fizz"}[input%3 == 0]
     // result += map[bool]string{true: "Buzz"}[input%5 == 0]
 	return map[bool]string{true: result, false: strconv.Itoa(input)}[result != ""]
 }
