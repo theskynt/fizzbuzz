0c099df4ebb61cbf1bb31a025be60efa6282518c
Narunart Tuntue <narunart.t@arise.tech>
2024-07-09 22:11:03 +0700

    [1]input 3 return Fizz

--- normal/main.go
+++ normal/main.go
@@ -3,5 +3,8 @@ package main
 import "strconv"
 
 func fizzbuzz(input int) string {
+	if input == 3 {
+		return "Fizz"
+	}
 	return strconv.Itoa(input)
 }
--- normal/main_test.go
+++ normal/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
-	t.Run("[1] input 1 return 1", func(t *testing.T) {
+	t.Run("input 1 return 1", func(t *testing.T) {
 		input := 1
 		want := "1"
 
@@ -14,9 +14,20 @@ func TestFizzBuzz(t *testing.T) {
 		}
 	})
 
-	t.Run("[2] input 2 return 2", func(t *testing.T) {
+	t.Run("input 2 return 2", func(t *testing.T) {
 		input := 2
 		want := "2"
+
+		got := fizzbuzz(input)
+
+		if got != want {
+			t.Errorf("Want : %v but got %v", want, got)
+		}
+	})
+
+	t.Run("input 3 return Fizz", func(t *testing.T) {
+		input := 3
+		want := "Fizz"
 
 		got := fizzbuzz(input)
 
