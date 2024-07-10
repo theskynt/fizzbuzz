b7ceedfbe33b0ad3161d65d258b1c3fd7ec13940
Narunart Tuntue <narunart.t@arise.tech>
2024-07-10 21:14:26 +0700

    [1] input 1 return 1

---
+++ normal/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	t.Run("input 1 return 1", func(t *testing.T) {
+		input := 1
+		want := "1"
+
+		got := fizzbuzz(input)
+
+		if got != want {
+			t.Errorf("Want: %v but got %v", want, got)
+		}
+	})
+
+}
