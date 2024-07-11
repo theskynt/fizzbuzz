6ba35a0396a78d25f70c0dedcfd2516a9b56fd42
Narunart Tuntue <narunart.t@arise.tech>
2024-07-11 23:40:42 +0700

    [1] input 7 return 7

--- normal/main_test.go
+++ normal/main_test.go
@@ -67,4 +67,15 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("Want: %v but got %v", want, got)
 		}
 	})
+
+	t.Run("input 7 return 7", func(t *testing.T) {
+		input := 7
+		want := "7"
+
+		got := fizzbuzz(input)
+
+		if got != want {
+			t.Errorf("Want: %v but got %v", want, got)
+		}
+	})
 }
