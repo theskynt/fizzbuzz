ddd71eaf25e057da862578dfedcb972d4dedb7b1
Narunart Tuntue <narunart.t@arise.tech>
2024-07-15 22:27:44 +0700

    [oneif][1] input 1 return 1

--- one_if/main.go
+++ one_if/main.go
@@ -3,11 +3,5 @@ package main
 import "strconv"
 
 func fizzbuzz(n int) string {
-	got := ""
-	got += map[bool]string{true: "Fizz"}[n%3 == 0]
-	got += map[bool]string{true: "Buzz"}[n%5 == 0]
-	if got == "" {
-		got = strconv.Itoa(n)
-	}
-	return got
+	return strconv.Itoa(n)
 }
