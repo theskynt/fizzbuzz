b4f276c5376760405b1f31f03c125da546bf5f61
Narunart Tuntue <narunart.t@arise.tech>
2024-07-15 22:28:01 +0700

    [oneif][1] input 2 return 2

--- one_if/main_test.go
+++ one_if/main_test.go
@@ -13,4 +13,15 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("want %v but got %v", want, got)
 		}
 	})
+
+	t.Run("input 2 return 2", func(t *testing.T) {
+		input := 2
+		want := "2"
+
+		got := fizzbuzz(input)
+
+		if got != want {
+			t.Errorf("want %v but got %v", want, got)
+		}
+	})
 }
