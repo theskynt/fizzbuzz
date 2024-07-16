f9d744eadaf97a31124520f040179f4ca572d683
Narunart Tuntue <narunart.t@arise.tech>
2024-07-16 21:31:41 +0700

    [noif][1] input 3 return Fizz

--- noif/main.go
+++ noif/main.go
@@ -3,5 +3,8 @@ package main
 import "strconv"
 
 func fizzbuzz(n int) string {
-	return strconv.Itoa(n)
+	got := ""
+	got += map[bool]string{true: "Fizz"}[n == 3]
+	got += map[bool]string{true: strconv.Itoa(n)}[got == ""]
+	return got
 }
