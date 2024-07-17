e51fb1c0398db92db4291f080d7e04fcf57f991a
Narunart Tuntue <narunart.t@arise.tech>
2024-07-17 23:07:30 +0700

    [oneif][1] input 10 return Buzz

--- one_if/main.go
+++ one_if/main.go
@@ -5,7 +5,7 @@ import "strconv"
 func fizzbuzz(n int) string {
 	got := ""
 	got += map[bool]string{true: "Fizz"}[n%3 == 0]
-	got += map[bool]string{true: "Buzz"}[n == 5]
+	got += map[bool]string{true: "Buzz"}[n%5 == 0]
 	if got == "" {
 		got += strconv.Itoa(n)
 	}
